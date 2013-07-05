d3209a71a22f93d4de326a70140630a8590937ce
Gh9063221 <zhengt.cn@gmail.com>
2013-07-05 12:39:27 +0800

    Initial Commit

---
+++ DLTabBarControllerDemo/DLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DLAppDelegate.h
+//  DLTabBarControllerDemo
+//
+//  Created by zht on 13-7-5.
+//  Copyright (c) 2013年 zht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
